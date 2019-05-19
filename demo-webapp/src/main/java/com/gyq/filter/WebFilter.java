07aa513348faec878d36946b7f75335431bf85f1
lovelypenguins <lovelypenguins262@gmail.com>
2019-05-20 01:27:25 +0800

    update the log configuration

--- demo-webapp/src/main/java/com/gyq/filter/WebFilter.java
+++ demo-webapp/src/main/java/com/gyq/filter/WebFilter.java
@@ -1,22 +1,29 @@
 package com.gyq.filter;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import java.io.IOException;
 
 public class WebFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(WebFilter.class);
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("filter init...");
+        logger.debug("filter init...");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         filterChain.doFilter(servletRequest, servletResponse);
-        System.out.println("do filter...");
+        logger.debug("do filter...");
     }
 
     @Override
     public void destroy() {
-        System.out.println("filter destory...");
+        logger.debug("filter destory...");
     }
 }
